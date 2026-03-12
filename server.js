e6191533c78ac411f55e28c8144af6b3ce7bad7f
basheerdraytonUB <basheer_drayton@unblindedmastery.com>
2026-03-12 15:37:54 -0400

    Update server.js

--- server.js
+++ server.js
@@ -2,6 +2,8 @@ import express from "express";
 import cors from "cors";
 import dotenv from "dotenv";
 import pkg from "jsrsasign";
+import path from "path";
+import { fileURLToPath } from "url";
 
 dotenv.config();
 
@@ -11,8 +13,13 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+app.use(express.static(path.join(__dirname, "public")));
+
 app.get("/", (req, res) => {
-  res.send("Zoom bot server running");
+  res.sendFile(path.join(__dirname, "public", "index.html"));
 });
 
 // Create Meeting SDK signature
